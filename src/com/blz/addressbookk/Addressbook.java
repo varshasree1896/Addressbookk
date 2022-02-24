ace99f5e6c3d898139852646140b83a59d495635
Pooja Sree <you@example.com>
2022-02-24 16:27:44 +0530

    Added:welcome message

---
+++ src/com/blz/addressbookk/Addressbook.java
@@ -0,0 +1,9 @@
+package com.blz.addressbookk;
+
+public class Addressbook {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address book program");
+	// write your code here
+    }
+}
