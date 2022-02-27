5273d02048b027edbe108993cd9aadfd354f2e10
Pooja Sree <you@example.com>
2022-02-28 05:00:28 +0530

    Added:UC5AddMultipleContct

--- src/com/blz/addressbookk/Addressbook.java
+++ src/com/blz/addressbookk/Addressbook.java
@@ -2,6 +2,7 @@ package com.blz.addressbookk;
 
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -11,18 +12,17 @@ import java.util.Scanner;
 
 
 public class Addressbook {
-    private ArrayList<Contact> contact = new ArrayList<>();
+    private static List<Contact> list = new ArrayList<Contact>();
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address book program");
         menu();
+        System.out.println(list);
     }
 
     private static void menu() {
         System.out.println(" 1. Add contact. ");
-        System.out.println(" 2. Edit contact. ");
-        System.out.println(" 3. Delete contact. ");
         System.out.println("Enter your Choice ");
         int choice = sc.nextInt();
         Addressbook contact = new Addressbook();
@@ -52,39 +52,19 @@ public class Addressbook {
                     choice = sc.nextInt();
                     }
                     break;
-                case 2:
-                    contact.editContact();
-                    break;
-                case 3:
-                    contact.deleteContact();
-                    break;
                     default:
                     System.out.println("Enter valid Number");
                     break;
             }
             System.out.println("1.Add contact.");
-            System.out.println("2.Edit contact");
-            System.out.println(" 3. Delete contact. ");
             choice = sc.nextInt();
         }
         System.out.println("Ended...Thank you");
     }
-    private String editContact() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Name of contact that you would like to edit: ");
-        String replacedName = sc.next();
-        System.out.println("Enter the updated name");
-        String newName = sc.next();
-        return (newName);
-    }
-    private String deleteContact() {
-        System.out.println("Which contact do you wish to remove?");
-        String removedContact = sc.next();
-        return (removedContact);
-    }
+
     private void addContact(String firstName, String lastName, String address, String city, String state, int zip, long phoneNumber, String email) {
         Contact person = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-        contact.add(person);
+        list.add(person);
         //person.display();
     }
 }
